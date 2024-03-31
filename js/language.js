0d025561c8ff9e132b0fa27c2c1ce30098e98fbf
wally <2842896077@qq.com>
2024-03-31 23:07:25 +0800

    多语言调整

--- js/language.js
+++ js/language.js
@@ -1,4 +1,4 @@
-function changeLanguage() {
+function help_changeLanguage() {
     var languageSelect = document.getElementById("languageSelect");
     var selectedLanguage = languageSelect.value;
 
@@ -7,5 +7,17 @@ function changeLanguage() {
     }
     else if (selectedLanguage === "cn") {
         window.location.href = "help(cn).html"; // 跳转到同一文件夹下的help(cn).html页面
+    }
+}
+
+function home_changeLanguage() {
+    var languageSelect = document.getElementById("languageSelect");
+    var selectedLanguage = languageSelect.value;
+
+    if (selectedLanguage === "en") {
+        window.location.href = "en.html"; // 跳转到同一文件夹下的en.html页面
+    }
+    else if (selectedLanguage === "cn") {
+        window.location.href = "index.html"; // 跳转到同一文件夹下的index.html页面
     }
 }
